860f69e51cbac2424a6cf5397e26e88760430f6a
Roseanne Gerin <rg2875@columbia.edu>
2014-04-25 18:19:17 -0400

    Third Commit

--- scripts.js
+++ scripts.js
@@ -11,104 +11,100 @@
 
 */
 
-//Add Google data fusion table.
-//Pass the data from the fusion table to the new DataLoaded () function with the name "CivilianUnemploymentData_030714."
-//Changed var names from "myKey" to "newKey" and "myTableURL" to "newTableURL""
+//Added Google data fusion table.
+//Passed the data from the fusion table to new "dataDriver()" function with the name "CivilianUnemploymentData_030714."
+//Changed var names from "myKey" to "newKey" and "myTableURL" to "newTableURL".
 
 var newKey = "&key=AIzaSyBxm3yDApl-FkVRwHUKGACLeBhMMTX2ubI";
 var newTableURL = "https://www.googleapis.com/fusiontables/v1/query?sql=SELECT+*+FROM+1sJdI-HjjB2yBpsxv9rzRjJwP7Y0HJYvkmjklpnDk+WHERE+DATE>";
 
-//Changed function name from "newDataLoaded" to "dataDriver"
+//Changed function name from "newDataLoaded" to "dataDriver".
 function dataDriver(UNRATE) {
 
-	//Changed var "googleDataSource" to "googleInfo"
+	//Changed var "googleDataSource" to "googleInfo".
 	var googleInfo = new google.visualization.DataTable();
 
-	//Add columns: The first parameter is the data type in that column.
+	//Added columns: The first parameter is the data type in that column.
 	//The second parameter is the name of the column.
+	//Don't forget to add rows after columns.
 
 	googleInfo.addColumn('string'), UNRATE.columns[0];
 	googleInfo.addColumn('number'), UNRATE.columns[1];
 	googleInfo.addColumn({type:'string', role:'tooltip', 'p': {'html': true}});
 	googleInfo.addRows(UNRATE.rows);
 	
-	//I have changed the var from "myChartOptions" to chartChanges""
+	//Changed var from "myChartOptions" to "chartChanges".
 	var chartChanges = {
-		title: "Unemployment in the United States"
+		title: "Unemployment in the United States since 1950"
 	};
 	
-	// Add this for googleInfo "tooltip" above: 
-		var ChartOptions = {
+	// Added this for googleInfo "tooltip" above: 
+		var chartInfo = { //Changed var from "ChartOptions" to "chartInfo".
 		width:600,
 		height:400,
 		tooltip: { isHtml: true}
 		
 	};
 	
-	//Create a line chart and pass the Google DataTable variable I've created here.
-	//Changed var "myGoogleChart" to "newChart"
+	//Created a line chart and passed the Google DataTable variable I've created here.
+	//Changed var "myGoogleChart" to "newChart".
 	var newChart = new google.visualization.LineChart(document.getElementById("unemploymentChartDiv"));
 	newChart.draw(googleInfo);
 
 }
 
-
-function showNewData(e){
+//Changed function from "showNewData" to "showInput".
+function showInput(e){
 	//e is my click event.
-	var myID = e.target.id; //e.g., "year 2000"
-	var myNameArray = myID.split("_"); //Splits into an array, "2000" will be second item.
-	var myYear = myNameArray[1]; //This grabs the year.
+	var newID = e.target.id; //e.g., "year 2000"; changed var "myID" to "newID".
+	var newNameArray = newID.split("_"); //Splits into an array, "2000" will be second item. 
+	//Changed var "MynameArray" to "newNameArray".
+	var latestYear = newNameArray[1]; //This grabs the year. Changed var "myYear" to "latestYear".
 	
-	$.get(newTableURL+"'"+myYear+"-12-01'"+newKey, dataDriver, "json");
+	$.get(newTableURL+"'"+latestYear+"-12-01'"+newKey, dataDriver, "json");
 
-	History.pushState({year:myYear}, "Unemployment from - "+myYear, "?year="+myYear);
+	History.pushState({year:latestYear}, "Unemployment from - "+latestYear, "?year="+latestYear);
 	
 }
 	
-	//Load the data from a Google fusion table instead of from the json file.
-	//Add the data file name and my API key ID to the https address.
-	// Add a WHERE parameter in the https address to specify certain years for the data.
-	// The WHERE parameter will display the chart data for 1949-12-01 and beyond.
-function googleLoaded() {
+	//Loaded the data from a Google fusion table instead of from the json file.
+	//Added the data file name and my API key ID to the https address.
+	// Changed the WHERE parameter to display the chart data for 1949-12-01 and beyond.
+	//Changed the function name from "googleLoaded" to "googleReady".
+function googleReady() {
 	
-	
-	//I have changed the "defaultYear" below to 1950 to add data from two more decades to the chart.
-	var urlData = History.getState().cleanUrl;
-	var queryArray = newTableURL.split("?");
-	var defaultYear = "1950";
-	if (queryArray.length > 1){
-		//Get the query string, break it on equals , and then take the right path which contains the year.
-		defaultYear = queryArray[1].split("=")[1];
+	//Changed the "defaultYear" below to 1950 to add data for two more decades to the chart.
+	var infoURL = History.getState().cleanUrl; //Changed var "urlData" to "infoURL".
+	var newArray = newTableURL.split("?");//Changed var "queryArray" to "newArray".
+	var newDefaultTime = "1950";//Changed var from "defaultYear" to "newDefaultTime".
+	if (newArray.length > 1){
+		//Get the query string, break it on equals, and then take the right path which contains the year.
+		newDefaultTime = newArray[1].split("=")[1];
 	}
 	
-	//var defaultYear = queryString.split("=");
-	//console.log(History.getStateByIndex(0));
-	//if($.isEmptyObject(urlData)){
-		//defaultYear = "1970";
-				
-
-	$(".btn-success").on("click", showNewData);
 	
-	//Grab the button with the ID that is year_"defaultYear"
+	$(".btn-success").on("click", showInput);
 	
-	$("#year_"+defaultYear).click();
+	//This will grab the button with the ID that is year_"newDefaultTime".
+	
+	$("#year_"+newDefaultTime).click();
 	
 	$.get(newTableURL+"'1949-12-01'"+newKey, dataDriver, "json");
 
 }
 
 	//I am working with the Google chart loading function.
-	function DataSource() {
+	//Changed function from "DataSource" to "downAndDirtyData".
+	function downAndDirtyData() {
 
-	//Load the Google vizualization library.
+	//Loaded the Google vizualization library. Changed the callback to "googleReady".
 	google.load("visualization", "1", {
 		packages : ["corechart"],
-		callback : "googleLoaded"
+		callback : "googleReady"
 
 	});
 
 }
 
-
-$(document).ready(DataSource);
+$(document).ready(downAndDirtyData);
 
